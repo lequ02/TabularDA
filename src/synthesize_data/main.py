2e972ff16e9f372173e6dc7add3a5628cd4baf50
unknown <zsomerandomdudez@gmail.com>
2024-06-21 00:59:47 +0700

    fix load_test_data bug

--- src/synthesize_data/main.py
+++ src/synthesize_data/main.py
@@ -1,4 +1,4 @@
-from synthesizer import synthesize_data, synthesize_from_trained_model
+from synthesizer import *
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -30,10 +30,15 @@ def main():
   #                            target_name='shares', synthesizer_file_name='../sdv trained model/news_synthesizer_onlyX.pkl',
   #                            csv_file_name='../data/news/news_sdv_categorical_100k.csv')
 
-  synthesize_adult = synthesize_from_trained_model(x_original, y_original, categorical_columns,
-                              sample_size=100_000, target_synthesizer='categoricalNB',
-                             target_name='income', synthesizer_file_name='../sdv trained model/adult/adult_synthesizer_onlyX.pkl',
-                             csv_file_name='../data/adult/adult_sdv_categorical_100k.csv')
+  # synthesize_adult = synthesize_from_trained_model(x_original, y_original, categorical_columns,
+  #                             sample_size=100_000, target_synthesizer='categoricalNB',
+  #                            target_name='income', synthesizer_file_name='../sdv trained model/adult/adult_synthesizer_onlyX.pkl',
+  #                           #  sdv trained model\news\news_synthesizer.pkl
+  #                            csv_file_name='../data/adult/adult_sdv_categorical_100k.csv')
+
+
+  news_synthesizer = load_synthesizer('../sdv trained model/news/news_synthesizer.pkl')
+  news_synthesizer.sample(100_000).to_csv('../data/news/Jun20_news_sdv_100k.csv')
 
 if __name__ == '__main__':
   main()
