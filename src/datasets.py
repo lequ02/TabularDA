9b1c1cbdb602c73e2323fd2c8d9694284ae0b558
Jibran Ahmed Khan <jibranahmed098@gmail.com>
2024-07-15 12:28:32 -0500

    update

--- src/datasets.py
+++ src/datasets.py
@@ -1,20 +1,21 @@
+
 import pandas as pd
-from ucimlrepo import fetch_ucirepo 
+from ucimlrepo import fetch_ucirepo
 
 def load_dataset(dataset_id, verbose=False):
     # fetch dataset
     dataset = fetch_ucirepo(id=dataset_id)
-    
+
     # data (as pandas dataframes)
     X = dataset.data.features
     y = dataset.data.targets
-    
+
     if verbose:
         # metadata
         print(dataset.metadata)
         # variable information
         print(dataset.variables)
-    
+
     return X, y
 
 def load_adult(verbose=False):
@@ -28,6 +29,11 @@ def load_adult(verbose=False):
 def load_news(verbose=False):
     return load_dataset(332, verbose)
 
+# x_news, y_news = load_news()
+# print("News dataset features shape:", x_news.shape)
+# print("News dataset target shape:", y_news.shape)
+
+
 def load_census(verbose=False):
     x, y = load_dataset(20, verbose)
     # the y values are <=50K, <=50K., >50K >50K.
@@ -35,3 +41,8 @@ def load_census(verbose=False):
     y['income'] = y['income'].str.replace('.', '', regex=False)
     # print(y.income.unique())
     return x, y
+
+
+
+
+
