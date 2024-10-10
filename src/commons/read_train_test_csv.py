b2518a54f4519916819e00de6fbbb52a10ef28e9
unknown <lequ02@luther.edu>
2024-10-11 02:59:02 +0700

    finish design adult.py; move onehot to commons

---
+++ src/commons/read_train_test_csv.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+
+def read_adult_data(train_csv, test_csv,
+                    target_name,
+                    categorical_columns):
+  """
+  read train and test data from csv files
+  """
+  
+  # check if the csv files exist
+  if not os.path.exists(train_csv) or not os.path.exists(test_csv):
+    raise FileNotFoundError("The csv file(s) do not exist")
+
+  # read adult data
+  train_data = pd.read_csv(train_csv)
+  xtrain = train_data.drop(columns=[target_name])
+  ytrain = train_data[target_name]
+
+  test_data = pd.read_csv(test_csv)
+  xtest = test_data.drop(columns=[target_name])
+  ytest = test_data[target_name]
+
+  return xtrain, xtest, ytrain, ytest, target_name, categorical_columns
--- src/synthesize_data/create_synthetic_data/adult.py
+++ src/synthesize_data/create_synthetic_data/adult.py
@@ -4,44 +4,18 @@ import pandas as pd
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from synthesizer import *
-from onehot import onehot
+# from onehot import onehot
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from datasets import load_adult, load_news, load_census, load_covertype
-from commons import create_train_test, handle_missing_values, check_directory
-
-def prepare_adult_data(save_train_as, save_test_as):
-  # process adult data
-  x_original, y_original = load_adult()
-  target_name = y_original.columns[0]
-  y_original = y_original['income'].map({'<=50K': 0, '>50K': 1})
-  categorical_columns = ['workclass', 'education', 'marital-status', 'occupation',
-                            'relationship', 'race', 'sex', 'native-country']
-
-  # train test split
-  data = pd.concat([x_original, y_original], axis=1)
-  xtrain, xtest, ytrain, ytest = create_train_test.create_train_test(data, target_name=target_name, test_size=0.2, random_state=42)
-
-  print(type(ytrain), ytrain.shape)
-  print(xtrain)
-  print(ytrain) 
-  xtrain, ytrain = handle_missing_values.handle_missing_values(xtrain, ytrain, target_name=target_name, strategy='drop')
-  xtest, ytest = handle_missing_values.handle_missing_values(xtest, ytest, target_name=target_name, strategy='drop')
-
-  print(xtrain.shape)
-  print(xtrain.isnull().sum())
-
-  train_set = pd.concat([xtrain, ytrain], axis=1)
-  test_set = pd.concat([xtest, ytest], axis=1)
-
-  check_directory.check_directory(save_train_as) # check if the directory exists, if not create it
-  check_directory.check_directory(save_test_as)
-  train_set.to_csv(save_train_as, index=False)
-  test_set.to_csv(save_test_as, index=False)
-
-  return xtrain, xtest, ytrain, ytest, target_name, categorical_columns
+from commons import create_train_test, handle_missing_values, check_directory, read_train_test_csv, onehot
 
 
 def create_synthetic_data_adult():
+  """
+  create train-test data for adult dataset
+  synthesize data using SDV only, SDV with GaussianNB, and SDV with CategoricalNB
+  """
+
   # synthesize data for the adult dataset
   paths = {'synthesizer_dir': '../sdv trained model/adult/',
             'data_dir': '../data/adult/',
@@ -59,31 +33,82 @@ def create_synthetic_data_adult():
             'sdv_categorical_csv': 'onehot_adult_sdv_categorical_100k.csv'
             }
 
-  # # sdv only
-  # xtrain, xtest, ytrain, ytest, target_name, categorical_columns = prepare_adult_data()
-  # xytrain = pd.concat([xtrain, ytrain], axis=1)
-  # synthesize_adult_sdv = synthesize_data(xytrain, ytrain, categorical_columns,
-  #                           sample_size=100_000, target_synthesizer='',
-  #                           target_name=target_name, synthesizer_file_name= paths['synthesizer_dir']+paths['sdv_only_synthesizer'],
-  #                           csv_file_name= paths['data_dir']+paths['sdv_only_csv'], verbose=True,
-  #                           # show_network=True
-  #                           )
+  # save train-test data to csv files
+  xtrain, xtest, ytrain, ytest, target_name, categorical_columns = prepare_train_test_adult(paths['data_dir']+paths['train_csv'], paths['data_dir']+paths['test_csv'])
+
+  # sdv only
+  xytrain = pd.concat([xtrain, ytrain], axis=1)
+  synthesize_adult_sdv = synthesize_data(xytrain, ytrain, categorical_columns,
+                            sample_size=100_000, target_synthesizer='',
+                            target_name=target_name, synthesizer_file_name= paths['synthesizer_dir']+paths['sdv_only_synthesizer'],
+                            csv_file_name= paths['data_dir']+paths['sdv_only_csv'], verbose=True,
+                            # show_network=True
+                            )
 
   # sdv gaussian
-  xtrain, xtest, ytrain, ytest, target_name, categorical_columns = prepare_adult_data(paths['data_dir']+paths['train_csv'], paths['data_dir']+paths['test_csv'])
+  xtrain, xtest, ytrain, ytest, target_name, categorical_columns = read_adult_data()
+  synthesize_adult_sdv_gaussian_100k = synthesize_data(xtrain, ytrain, categorical_columns,
+                            sample_size=100_000, target_synthesizer='gaussianNB',
+                            target_name=target_name, synthesizer_file_name= paths['synthesizer_dir']+paths['sdv_gaussian_synthesizer'],
+                            csv_file_name= paths['data_dir']+paths['sdv_gaussian_csv'], verbose=True,
+                            # show_network=True
+                            )
 
-  # synthesize_adult_sdv_gaussian_100k = synthesize_data(xtrain, ytrain, categorical_columns,
-  #                           sample_size=100_000, target_synthesizer='gaussianNB',
-  #                           target_name=target_name, synthesizer_file_name= paths['synthesizer_dir']+paths['sdv_gaussian_synthesizer'],
-  #                           csv_file_name= paths['data_dir']+paths['sdv_gaussian_csv'], verbose=True,
-  #                           # show_network=True
-  #                           )
+  # sdv categorical
+  xtrain, xtest, ytrain, ytest, target_name, categorical_columns = read_adult_data()
+  synthesize_adult_sdv_categorical_100k = synthesize_from_trained_model(xtrain, ytrain, categorical_columns,
+                            sample_size=100_000, target_synthesizer='categoricalNB',
+                            target_name=target_name, synthesizer_file_name= paths['synthesizer_dir']+paths['sdv_categorical_synthesizer'],
+                            csv_file_name= paths['data_dir']+paths['sdv_categorical_csv'], verbose=True,
+                            # show_network=True
+                            )
 
-  # # sdv categorical
-  # xtrain, xtest, ytrain, ytest, target_name, categorical_columns = prepare_adult_data()
-  # synthesize_adult_sdv_categorical_100k = synthesize_from_trained_model(xtrain, ytrain, categorical_columns,
-  #                           sample_size=100_000, target_synthesizer='categoricalNB',
-  #                           target_name=target_name, synthesizer_file_name= paths['synthesizer_dir']+paths['sdv_categorical_synthesizer'],
-  #                           csv_file_name= paths['data_dir']+paths['sdv_categorical_csv'], verbose=True,
-  #                           # show_network=True
-  #                           )
+
+def prepare_train_test_adult(save_train_as, save_test_as):
+  """
+  map the y value to 0 and 1
+  train-test split the adult data
+  handle missing values
+  save the train-test data to csv
+  train-test csv files are NOT one-hot encoded
+  """
+  # process adult data
+  x_original, y_original = load_adult()
+  target_name = y_original.columns[0]
+  y_original = y_original['income'].map({'<=50K': 0, '>50K': 1})
+  categorical_columns = ['workclass', 'education', 'marital-status', 'occupation',
+                            'relationship', 'race', 'sex', 'native-country']
+
+  # train test split
+  data = pd.concat([x_original, y_original], axis=1)
+  xtrain, xtest, ytrain, ytest = create_train_test.create_train_test(data, target_name=target_name, test_size=0.2, random_state=42)
+
+  # handle missing values
+  xtrain, ytrain = handle_missing_values.handle_missing_values(xtrain, ytrain, target_name=target_name, strategy='drop')
+  xtest, ytest = handle_missing_values.handle_missing_values(xtest, ytest, target_name=target_name, strategy='drop')
+
+  # save train-test to csv
+  train_set = pd.concat([xtrain, ytrain], axis=1)
+  test_set = pd.concat([xtest, ytest], axis=1)
+  check_directory.check_directory(save_train_as) # check if the directory exists, if not create it
+  check_directory.check_directory(save_test_as)
+  train_set.to_csv(save_train_as, index=False)
+  test_set.to_csv(save_test_as, index=False)
+
+  return xtrain, xtest, ytrain, ytest, target_name, categorical_columns
+
+
+def read_adult_data():
+  """
+  read train and test data from csv files
+  """
+  train_csv="..\\data\\adult\\adult_train.csv"
+  test_csv="..\\data\\adult\\adult_test.csv"
+  target_name='income'
+  categorical_columns=['workclass', 'education', 'marital-status', 'occupation',
+                      'relationship', 'race', 'sex', 'native-country']
+
+  xtrain, xtest, ytrain, ytest, target_name, categorical_columns = read_train_test_csv.read_train_test_csv(train_csv, test_csv,
+   target_name=target_name, categorical_columns=categorical_columns)
+
+  return xtrain, xtest, ytrain, ytest, target_name, categorical_columns
