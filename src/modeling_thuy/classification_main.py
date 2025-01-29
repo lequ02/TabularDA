89c1bc7aa88027783dda94b4a34e47c81a128fcc
dahalSabina <dahasa01@luther.edu>
2025-01-29 14:01:09 -0600

    plots done for adult and census

--- src/modeling_thuy/classification_main.py
+++ src/modeling_thuy/classification_main.py
@@ -10,7 +10,7 @@ def parse_args():
 #data
   parser.add_argument('--dataset-name', dest='dataset_name', help='Name of the dataset (e.g., adult, census, intrusion)', type=str, required=True)
   parser.add_argument('--train-option', dest='train_option', help='Training data option (original, synthetic or mix)', type=str, required=True)
-  parser.add_argument('--augment-option', dest='augment_option', help='Synthetic data option (ctgan or gaussian or categorical)', type=str, default=None)
+  parser.add_argument('--augment-option', dest='augment_option', help='Synthetic data option (ctgan or gaussian or categorical or pca_gmm)', type=str, default=None)
   parser.add_argument('--mix-ratio', dest='mix_ratio', help='When train_option = mix, you need to specify the percentage|#samples to take from the original data', type=float, default=-1)
   parser.add_argument('--n-sample', dest='n_sample', help='mix_ratio is specified, you need to specify #samples for the whole train set', type=int, default=-1)
   parser.add_argument('--test-option', dest='test_option', help='Test data option (original or mix)', type=str, required=True)
@@ -33,7 +33,11 @@ def main(args):
 
   w_dir = OUT_DIR + dataset_name + "/weight/"
   acc_dir = OUT_DIR + dataset_name + "/acc/"
+<<<<<<< HEAD
   eval_metrics = {"accuracy": None, "f1": ['macro', 'micro', 'weighted']} # add 'binary' to the list for binary classification (i.e: adult, census, credit)
+=======
+  eval_metrics = {"accuracy": None, "f1": ['macro', 'micro', 'weighted', 'binary']} # add 'binary' to the list for binary classification (i.e: adult, census, credit)
+>>>>>>> ed8ca30ba (plots done for adult and census)
   metric_to_plot = "f1_micro"
 
 
--- src/modeling_thuy/models_folder/model_census.py
+++ src/modeling_thuy/models_folder/model_census.py
@@ -54,43 +54,58 @@ class DNN_Census(nn.Module):
         return self
     
 
+   #CTGAN 
 # class DNN_Census_mix(nn.Module):
-#     def __init__(self, input_size, hidden_sizes=[32, 16, 16], output_size=1):
+#     def __init__(self, input_size, hidden_sizes=[512,256,256,128, 64, 32], output_size=1):
 #         super(DNN_Census_mix, self).__init__()
         
 #         # First Hidden Layer
 #         self.layer1 = nn.Linear(input_size, hidden_sizes[0])
 #         self.bn1 = nn.BatchNorm1d(hidden_sizes[0])
 #         self.act1 = nn.ReLU()
-#         self.drop1 = nn.Dropout(0.8)
+#         self.drop1 = nn.Dropout(0.7)
         
 #         # Second Hidden Layer
 #         self.layer2 = nn.Linear(hidden_sizes[0], hidden_sizes[1])
 #         self.bn2 = nn.BatchNorm1d(hidden_sizes[1])
 #         self.act2 = nn.ReLU()
-#         self.drop2 = nn.Dropout(0.8)
+#         self.drop2 = nn.Dropout(0.7)
         
 #         # Third Hidden Layer
 #         self.layer3 = nn.Linear(hidden_sizes[1], hidden_sizes[2])
 #         self.bn3 = nn.BatchNorm1d(hidden_sizes[2])
 #         self.act3 = nn.ReLU()
-#         self.drop3 = nn.Dropout(0.8)
+#         self.drop3 = nn.Dropout(0.7)
 
-#         # # Fourth Hidden Layer
-#         # self.layer4 = nn.Linear(hidden_sizes[2], hidden_sizes[3])
-#         # self.bn4 = nn.BatchNorm1d(hidden_sizes[3])
-#         # self.act4 = nn.ReLU()
-#         # self.drop4 = nn.Dropout(0.5)
+#         # Fourth Hidden Layer
+#         self.layer4 = nn.Linear(hidden_sizes[2], hidden_sizes[3])
+#         self.bn4 = nn.BatchNorm1d(hidden_sizes[3])
+#         self.act4 = nn.ReLU()
+#         self.drop4 = nn.Dropout(0.7)
+
+#         # Fifth Hidden Layer
+#         self.layer5 = nn.Linear(hidden_sizes[3], hidden_sizes[4])
+#         self.bn5 = nn.BatchNorm1d(hidden_sizes[4])
+#         self.act5 = nn.ReLU()
+#         self.drop5 = nn.Dropout(0.7)
+
+#         # Sixth Hidden Layer
+#         self.layer6 = nn.Linear(hidden_sizes[4], hidden_sizes[5])
+#         self.bn6 = nn.BatchNorm1d(hidden_sizes[5])
+#         self.act6 = nn.ReLU()
+#         self.drop6 = nn.Dropout(0.7)
 
 #         # Output Layer
-#         self.output = nn.Linear(hidden_sizes[2], output_size)
+#         self.output = nn.Linear(hidden_sizes[5], output_size)
 #         self.sigmoid = nn.Sigmoid()
 
 #     def forward(self, x):
 #         x = self.drop1(self.act1(self.bn1(self.layer1(x))))
 #         x = self.drop2(self.act2(self.bn2(self.layer2(x))))
 #         x = self.drop3(self.act3(self.bn3(self.layer3(x))))
-#         # x = self.drop4(self.act4(self.bn4(self.layer4(x))))
+#         x = self.drop4(self.act4(self.bn4(self.layer4(x))))
+#         x = self.drop5(self.act5(self.bn5(self.layer5(x))))
+#         x = self.drop6(self.act6(self.bn6(self.layer6(x))))
 #         x = self.output(x)
 #         x = self.sigmoid(x)  # output probabilities
 #         return x
@@ -99,21 +114,24 @@ class DNN_Census(nn.Module):
 #         super().train(mode)
 #         return self
     
+ 
+ #Gaussian
+
 class DNN_Census_mix(nn.Module):
-    def __init__(self, input_size, hidden_sizes=[64, 32, 16], output_size=1):
+    def __init__(self, input_size, hidden_sizes=[512,128, 64, 32], output_size=1):
         super(DNN_Census_mix, self).__init__()
         
         # First Hidden Layer
         self.layer1 = nn.Linear(input_size, hidden_sizes[0])
         self.bn1 = nn.BatchNorm1d(hidden_sizes[0])
         self.act1 = nn.ReLU()
-        self.drop1 = nn.Dropout(0.7)
+        self.drop1 = nn.Dropout(0.8)
         
         # Second Hidden Layer
         self.layer2 = nn.Linear(hidden_sizes[0], hidden_sizes[1])
         self.bn2 = nn.BatchNorm1d(hidden_sizes[1])
         self.act2 = nn.ReLU()
-        self.drop2 = nn.Dropout(0.7)
+        self.drop2 = nn.Dropout(0.8)
         
         # Third Hidden Layer
         self.layer3 = nn.Linear(hidden_sizes[1], hidden_sizes[2])
@@ -121,21 +139,35 @@ class DNN_Census_mix(nn.Module):
         self.act3 = nn.ReLU()
         self.drop3 = nn.Dropout(0.7)
 
-        # # Fourth Hidden Layer
-        # self.layer4 = nn.Linear(hidden_sizes[2], hidden_sizes[3])
-        # self.bn4 = nn.BatchNorm1d(hidden_sizes[3])
-        # self.act4 = nn.ReLU()
-        # self.drop4 = nn.Dropout(0.5)
+        # Fourth Hidden Layer
+        self.layer4 = nn.Linear(hidden_sizes[2], hidden_sizes[3])
+        self.bn4 = nn.BatchNorm1d(hidden_sizes[3])
+        self.act4 = nn.ReLU()
+        self.drop4 = nn.Dropout(0.7)
+
+        # # Fifth Hidden Layer
+        # self.layer5 = nn.Linear(hidden_sizes[3], hidden_sizes[4])
+        # self.bn5 = nn.BatchNorm1d(hidden_sizes[4])
+        # self.act5 = nn.ReLU()
+        # self.drop5 = nn.Dropout(0.7)
+
+        # # Sixth Hidden Layer
+        # self.layer6 = nn.Linear(hidden_sizes[4], hidden_sizes[5])
+        # self.bn6 = nn.BatchNorm1d(hidden_sizes[5])
+        # self.act6 = nn.ReLU()
+        # self.drop6 = nn.Dropout(0.7)
 
         # Output Layer
-        self.output = nn.Linear(hidden_sizes[2], output_size)
+        self.output = nn.Linear(hidden_sizes[3], output_size)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.drop1(self.act1(self.bn1(self.layer1(x))))
         x = self.drop2(self.act2(self.bn2(self.layer2(x))))
         x = self.drop3(self.act3(self.bn3(self.layer3(x))))
-        # x = self.drop4(self.act4(self.bn4(self.layer4(x))))
+        x = self.drop4(self.act4(self.bn4(self.layer4(x))))
+        # x = self.drop5(self.act5(self.bn5(self.layer5(x))))
+        # x = self.drop6(self.act6(self.bn6(self.layer6(x))))
         x = self.output(x)
         x = self.sigmoid(x)  # output probabilities
         return x
